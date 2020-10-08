878354dabd4b219d2142307ea7b4adbf2ba3ca55
nurtt0949 <71143068+nurtt0949@users.noreply.github.com>
2020-10-08 15:21:15 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Shows dot product for vectors
+ * @author Tolga Nurtekin
  */
 public class Main {
 
@@ -9,7 +10,32 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //create a scanner
+    Scanner input = new Scanner(System.in);
+    //create variable array for first vector
+    int [] vector = new int[3];
+    //create variable for second vector
+    int [] vector2 = new int[3];
+    //ask user for first vector values
+    System.out.println("Please enter the 3 values for the first vector");
+    //put integers into array into for loop
+    for(int i = 0; i < 3; i++){
+      vector[i] = input.nextInt();
+    } 
+    //ask user for second vector values
+    System.out.println("Please enter the 3 values for the second vector");
+    //put integer into array into for loop
+     for(int i = 0; i < 3; i++){
+      vector2[i] = input.nextInt();
+     }
+     //create variable for total 
+     int total = 0;
+     //create a for dot product
+     for(int i = 0; i < vector.length; i++){
+       int sum = vector[i] * vector2[i];
+       total = total + sum;
+     } 
+      //let user know their dot product
+     System.out.println("The dot product is " + total);
   }
 }
